07475b4d8b48a6373d9ab524f5bcd9b17bdd058f
J Perrin <perrinjack96@gmail.com>
2020-09-21 14:57:30 +0100

    Removed harcoded URLs to using namespace

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
+
 
 from .models import Question
 
