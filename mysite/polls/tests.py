895c9b5f4bc44cd904d267fc4d584b80ce7c2438
J Perrin <perrinjack96@gmail.com>
2020-09-30 17:34:04 +0100

    Test: Multiple past wustions displayed on index page

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -50,18 +50,30 @@ class QuestionIndexViewTests(TestCase):
     self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
 
-     def test_future_question_and_past_question(self):
-        """
-        Even if both past and future questions exist, only past questions
-        are displayed.
-        """
-        create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse('polls:index'))
-        self.assertQuerysetEqual(
-            response.context['latest_question_list'],
-            ['<Question: Past question.>']
-        )
+    def test_future_question_and_past_question(self):
+    """
+    Even if both past and future questions exist, only past questions
+    are displayed.
+    """
+    create_question(question_text="Past question.", days=-30)
+    create_question(question_text="Future question.", days=30)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(
+        response.context['latest_question_list'],
+        ['<Question: Past question.>']
+    )
+
+    def test_two_past_questions(self):
+    """
+    The questions index page may display multiple questions.
+    """
+    create_question(question_text="Past question 1.", days=-30)
+    create_question(question_text="Past question 2.", days=-5)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(
+        response.context['latest_question_list'],
+        ['<Question: Past question 2.>', '<Question: Past question 1.>']
+    )
 
 
 class QuestionModelTests(TestCase):
